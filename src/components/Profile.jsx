aa7d33c15de75e847197c6d2d76c3e6f38eccd8e
Decemmber <eu.krimp29@gmail.com>
2019-07-31 01:33:51 +0700

    First commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return             <div className='content'>
+    <div className='content__background'>
+    <img src="https://www.abstechnology.com/wp-content/uploads/2018/01/Cloud-Infrastructure-Assessment.jpg" alt=""/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+  </div>
+}
+
+export default Profile;
